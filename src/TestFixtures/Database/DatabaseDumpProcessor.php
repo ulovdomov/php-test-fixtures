ca00be771335da9df44a60b062638670361f33d3
mesour <matous.nemec@gmail.com>
2024-11-27 12:47:32 +0100

    Fix CI

--- src/TestFixtures/Database/DatabaseDumpProcessor.php
+++ src/TestFixtures/Database/DatabaseDumpProcessor.php
@@ -20,8 +20,9 @@ final class DatabaseDumpProcessor
             $process->run();
 
             if (!$process->isSuccessful()) {
-                Debugger::log($process->getOutput() . $process->getErrorOutput(), Debugger::WARNING);
-                throw new \LogicException('Problem with generating cache for database migrations in tests');
+                $message = $process->getOutput() . $process->getErrorOutput();
+                Debugger::log($message, Debugger::WARNING);
+                throw new \LogicException('Problem with generating cache for database migrations in tests: ' . $message);
             }
         } catch (\Throwable $e) {
             throw new \LogicException($e->getMessage(), $e->getCode(), $e);
@@ -36,8 +37,9 @@ final class DatabaseDumpProcessor
             $process->run();
 
             if (!$process->isSuccessful()) {
-                Debugger::log($process->getOutput() . $process->getErrorOutput(), Debugger::WARNING);
-                throw new \LogicException('Problem with importing cache with database migrations in tests');
+                $message = $process->getOutput() . $process->getErrorOutput();
+                Debugger::log($message, Debugger::WARNING);
+                throw new \LogicException('Problem with importing cache with database migrations in tests: ' . $message);
             }
         } catch (\Throwable $e) {
             throw new \LogicException($e->getMessage(), $e->getCode(), $e);
